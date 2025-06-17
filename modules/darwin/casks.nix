52bcab42c9476a9e7ceca0b7114c1908a4a8acc4
Michael Mroz <michaelm@mercury.com>
2025-06-16 21:16:40 -0700

    no more neovide

--- modules/darwin/casks.nix
+++ modules/darwin/casks.nix
@@ -8,7 +8,7 @@ _:
   "visual-studio-code" # Code Editor
   "cursor" # Code editor with advanced AI features
   "zed" # Code Editor
-  "neovide" # Modern GUI for neovim
+  # "neovide" # Modern GUI for neovim - i need to fix something... neovim version? Also it took over my default apps.
   "texifier" # LaTeX Editor
   "texstudio" # LaTeX Editor
 

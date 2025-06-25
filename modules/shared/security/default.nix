e426510f1869749785e404194b0c84bdbb12803d
Michael Mroz <michaelm@mercury.com>
2025-06-25 14:13:47 -0700

    add garnix key

--- modules/shared/security/default.nix
+++ modules/shared/security/default.nix
@@ -7,6 +7,7 @@
       "https://mroz.cachix.org"
       "https://cache.mercury.com"
       "https://hyprland.cachix.org"
+      "https://cache.garnix.io"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
@@ -14,6 +15,7 @@
       "mroz.cachix.org-1:yHi4Z+V6BviriR92yRIKFSfo6QR2LBSH7/YALi/f6vQ="
       "cache.mercury.com:yhfFlgvqtv0cAxzflJ0aZW3mbulx4+5EOZm6k3oML+I="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
   };
 

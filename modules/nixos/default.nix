9423d9eb60c92a312f4375d76bd061ef239c5c92
Michael Mroz <4539332+mjvmroz@users.noreply.github.com>
2025-08-05 22:43:41 -0700

    tokyo1958 looking pretty tidy

---
+++ modules/nixos/default.nix
@@ -0,0 +1,18 @@
+{ inputs }:
+{
+  imports = [
+    ./hardware/nvidia.nix
+    ./hardware/samsung-odyssey.nix
+    inputs.home-manager.nixosModules.home-manager
+    inputs.stylix.nixosModules.stylix
+    ./core.nix
+    ./x11.nix
+    ./localization.nix
+    ./security.nix
+    ../shared/security
+    ../shared/fonts.nix
+    ./gaming.nix
+    ./hyprland.nix
+    ./audio.nix
+  ];
+}

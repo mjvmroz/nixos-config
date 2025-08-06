402ec6255f22ed756de6de60224daea581650ea0
Michael Mroz <4539332+mjvmroz@users.noreply.github.com>
2025-08-05 22:43:48 -0700

    update flake, add carapace

--- modules/shared/packages.nix
+++ modules/shared/packages.nix
@@ -18,6 +18,7 @@ with pkgs;
 
   # General packages for development and system management
   silver-searcher # Nice search tool
+  carapace
   aspell # Spell checker
   aspellDicts.en # English dictionary
   bash-completion # Better bash completion

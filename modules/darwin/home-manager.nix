0cb96e1b5ba5f8d2d37dbc65bddb83842323a96a
Michael Mroz <4539332+mjvmroz@users.noreply.github.com>
2025-08-05 22:43:20 -0700

    dropped workaround for home manager builds

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -86,10 +86,6 @@ in
               gpgSshProgram
               ;
           };
-
-        # Marked broken Oct 20, 2022 check later to remove this
-        # https://github.com/nix-community/home-manager/issues/3344
-        manual.manpages.enable = false;
       };
   };
 

84244d9365a4f3b54ec5c716363c749a303226c8
Michael Mroz <4539332+mjvmroz@users.noreply.github.com>
2025-08-05 22:43:18 -0700

    nuke apps, update readme, update flake

--- flake.nix
+++ flake.nix
@@ -92,31 +92,9 @@
               '';
             };
         };
-      mkApp = scriptName: system: {
-        type = "app";
-        program = "${
-          (nixpkgs.legacyPackages.${system}.writeScriptBin scriptName ''
-            #!/usr/bin/env bash
-            PATH=${nixpkgs.legacyPackages.${system}.git}/bin:$PATH
-            echo "Running ${scriptName} for ${system}"
-            exec ${self}/apps/${system}/${scriptName}
-          '')
-        }/bin/${scriptName}";
-      };
-      mkLinuxApps = system: {
-        "build-switch" = mkApp "build-switch" system;
-      };
-      mkDarwinApps = system: {
-        "build" = mkApp "build" system;
-        "build-switch" = mkApp "build-switch" system;
-        "rollback" = mkApp "rollback" system;
-      };
     in
     {
       devShells = forAllSystems devShell;
-      apps =
-        nixpkgs.lib.genAttrs linuxSystems mkLinuxApps
-        // nixpkgs.lib.genAttrs darwinSystems mkDarwinApps;
 
       darwinConfigurations =
         nixpkgs.lib.genAttrs darwinSystems (

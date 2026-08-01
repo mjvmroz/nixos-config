4b328b0db3eac0820050fa8630985a1aecdd3ded
Michael Mroz <4539332+mjvmroz@users.noreply.github.com>
2026-08-01 11:48:46 -0700

    betterdisplay fixes things - the personal m2 max gets weird flickery issues with external displays

--- modules/darwin/packages.nix
+++ modules/darwin/packages.nix
@@ -5,3 +5,6 @@ let
   shared-packages = import ../shared/packages.nix { inherit pkgs; };
 in
 shared-packages
+++ [
+  betterdisplay # Display management; used to disable GPU dithering to fix external monitor flicker
+]
